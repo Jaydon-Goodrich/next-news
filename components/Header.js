f39f7f7bc49c5d7e0ae7a259b9259aff6eb88887
Jaydon Goodrich <jaydongoodrich@gmail.com>
2021-02-16 23:52:20 -0700

    simple news

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='title'><span>Next</span> News</h1>
+            <p>Keep up to date with the news</p>
+        </div>
+    )
+}
+
+export default Header;
